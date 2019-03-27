8b288c3c59bebe264ec4f24b2c9e5a069a9ad009
ganimaedes <jenmedal1000@gmx.fr>
2019-03-27 13:42:26 -0400

    indentation changes

--- src/main.cpp
+++ src/main.cpp
@@ -17,27 +17,27 @@ const char *showWD()
 
 void printHelp()
 {
-	std::cout << "-------Help For Search Script-------\n\n"
-			  << "To Execute Program, Type: \n" 
-		 	  << "./search Followed by These Options:\n\n"
-		 	  << "-n : \t\t\t\t Specify a File Name to Search\n\n"
-		 	  << "-e : \t\t\t\t Specify a File Extension\n\n"
-		 	  << "-s : \t\t\t\t Specify a String to Search in a File, \n\n"
-		 	  << "     \t\t\t\t Note : The File Name Or File Extension Must be Specified!\n\n"
-		 	  << "-p : \t\t\t\t Print All Files and Folders Inside a Directory\n\n"
-		 	  << "-d : \t\t\t\t Specify a Directory to Search Into\n\n";
-	std::cout << "Examples:\n"
-			  << "./search -e \"txt\" -s \"cmake\" -d \"$HOME/Documents\"\n"
-			  << "  ^       ^          ^            ^\n"
-			  << "  ¦       ¦          ¦            ¦-----> Search Within this Directory\n"
-			  << "  ¦       ¦          ¦-----> Search the String \"cmake\" in All the Files Found With Extension \".txt\"\n"
-			  << "  ¦       ¦-----> Search for extension \".txt\"\n"
-			  << "  ¦---> Name of Program\n\n";
-	std::cout << "./search -n \"CMake\" -d \"$HOME/Documents\"\n"
-			  << "  ^       ^            ^\n"
-			  << "  ¦       ¦            ¦-----> Search Within this Directory\n"
-			  << "  ¦       ¦-----> Search for This String in File Name\n"
-			  << "  ¦---> Name of Program\n";
+    std::cout << "-------Help For Search Script-------\n\n"
+              << "To Execute Program, Type: \n" 
+              << "./search Followed by These Options:\n\n"
+              << "-n : \t\t\t\t Specify a File Name to Search\n\n"
+              << "-e : \t\t\t\t Specify a File Extension\n\n"
+              << "-s : \t\t\t\t Specify a String to Search in a File, \n\n"
+              << "     \t\t\t\t Note : The File Name Or File Extension Must be Specified!\n\n"
+              << "-p : \t\t\t\t Print All Files and Folders Inside a Directory\n\n"
+              << "-d : \t\t\t\t Specify a Directory to Search Into\n\n";
+    std::cout << "Examples:\n"
+              << "./search -e \"txt\" -s \"cmake\" -d \"$HOME/Documents\"\n"
+              << "  ^       ^          ^            ^\n"
+              << "  ¦       ¦          ¦            ¦-----> Search Within this Directory\n"
+              << "  ¦       ¦          ¦-----> Search the String \"cmake\" in All the Files Found With Extension \".txt\"\n"
+              << "  ¦       ¦-----> Search for extension \".txt\"\n"
+              << "  ¦---> Name of Program\n\n";
+    std::cout << "./search -n \"CMake\" -d \"$HOME/Documents\"\n"
+              << "  ^       ^            ^\n"
+              << "  ¦       ¦            ¦-----> Search Within this Directory\n"
+              << "  ¦       ¦-----> Search for This String in File Name\n"
+              << "  ¦---> Name of Program\n";
 }
 
 int main(int argc, char *argv[])
@@ -47,35 +47,35 @@ int main(int argc, char *argv[])
     //./main2 -e "txt" -s "cmake" -d "$HOME/qwtq",./main2 -n "CMake" -d "$HOME/qwtq"
     // verifier si pour -e ".txt" est mis et l'enlever
     for (int i = 0; i < argc; ++i) {
-        if (strcmp(argv[i], "-n") == 0) {                                           	// -n fileName
+        if (strcmp(argv[i], "-n") == 0) {                                           	        // -n fileName
             if (i < argc - 1) {
                 fileName = argv[i + 1];
             } else {
                 std::cerr << "Missing argument for file name\n";
                 exit(-1);
             }
-        } else if (strcmp(argv[i], "-e") == 0) {                                    	// -e fileExtension
+        } else if (strcmp(argv[i], "-e") == 0) {                                    	        // -e fileExtension
             if (i < argc - 1) {
                 fileExtension = argv[i + 1];
             } else {
                 std::cerr << "Missing argument for file extension\n";
                 exit(-1);
             }
-        } else if (strcmp(argv[i], "-s") == 0) {                                    	// -s string to search in found file
+        } else if (strcmp(argv[i], "-s") == 0) {                                    	        // -s string to search in found file
             if (i < argc - 1) {
                 strInFile = argv[i + 1];
             } else {
                 std::cerr << "Missing argument for string to search in file\n";
                 exit(-1);
             }
-        } else if (strcmp(argv[i], "-p") == 0) {										// -p print all files and folders in directory
+        } else if (strcmp(argv[i], "-p") == 0) {					        // -p print all files and folders in directory
         	if (i < argc - 1) {
-        		printAll = argv[i + 1];
+                    printAll = argv[i + 1];
         	} else {
-        		std::cerr << "Missing argument for printing all files and folders\n";
-        		exit(-1);
+                    std::cerr << "Missing argument for printing all files and folders\n";
+                    exit(-1);
         	}
-        } else if (strcmp(argv[i], "-d") == 0) {                                    	// -d directory to search in
+        } else if (strcmp(argv[i], "-d") == 0) {                                    	        // -d directory to search in
             if (i < argc - 1) {
                 directory = argv[i + 1];
             } else {
@@ -86,29 +86,29 @@ int main(int argc, char *argv[])
         	printHelp();
         }  
         if (i == argc - 1) {
-        	if (directory.empty()) {
-        		if (strlen(argv[1]) != 0) {
-        			directory = argv[1];
-        		} else if (strlen(argv[1]) == 0) {
-        			directory = showWD();
-        		} else {
-					std::cerr << "Error: A directory must be specified with \"-d\"\n";
-        			exit(-1);
-        		}
-        	}
-        	if (!strInFile.empty() && fileName.empty() && fileExtension.empty()) {
-        		std::cerr << "Please specify a file name to search with \"-n\"" 
-        				  << " OR a file extension with \"-e\"\n";
-				exit(-1);
-        	}
+            if (directory.empty()) {
+                if (strlen(argv[1]) != 0) {
+                    directory = argv[1];
+                } else if (strlen(argv[1]) == 0) {
+                    directory = showWD();
+                } else {
+                    std::cerr << "Error: A directory must be specified with \"-d\"\n";
+                    exit(-1);
+                }
+            }
+            if (!strInFile.empty() && fileName.empty() && fileExtension.empty()) {
+                std::cerr << "Please specify a file name to search with \"-n\"" 
+                          << " OR a file extension with \"-e\"\n";
+                exit(-1);
+            }
         } 
     }
     std::cout << "fileName.length() = " << fileName.length() << "  fileExtension.length() = " << fileExtension.length()
               << "\ndirectory.length() = " << directory.length() << "  strInFile.length() = " << strInFile.length() << "\n";
 	
 	
-	std::vector<Directories> array;
-	std::string prevDir;
+    std::vector<Directories> array;
+    std::string prevDir;
     if (!directory.empty() && fileName.empty() && fileExtension.empty() && strInFile.empty()) {
         std::cout << "1\n";
         Parcours(const_cast<char *>(directory.c_str()), 0, array, "", ""); 
--- src/print.cpp
+++ src/print.cpp
@@ -2,11 +2,11 @@
 
 Print::Print(std::vector<Directories> &array) : m_array(array) 
 {
-	//std::string prevDir;
-	//std::cout << "m_array.size() = " << m_array.size() << "\n";
-	for (std::size_t i = 0; i < m_array.size(); ++i) {
-		for (int k = 0; k < m_array[i].indent + 1; ++k) { std::cout << "  "; }
-		std::cout << m_array[i].name << "\n";	
-	}	
+    //std::string prevDir;
+    //std::cout << "m_array.size() = " << m_array.size() << "\n";
+    for (std::size_t i = 0; i < m_array.size(); ++i) {
+        for (int k = 0; k < m_array[i].indent + 1; ++k) { std::cout << "  "; }
+        std::cout << m_array[i].name << "\n";	
+    }	
 }
 
